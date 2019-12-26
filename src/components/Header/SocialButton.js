b15a301b2dbe2320c644a7cb6e1c62da3832944a
Taek2 <zmn5119@gmail.com>
2019-12-26 19:02:33 +0900

    refactor:  switch statement

--- src/components/Header/SocialButton.js
+++ src/components/Header/SocialButton.js
@@ -1,7 +1,38 @@
-import React from "react";
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
 
-function SocialButton() {
-  return <div>github</div>;
+import Github from "../Icon/GithubIcon";
+import Facebook from "../Icon/FacebookIcon";
+import LinkedIn from "../Icon/LinkedInIcon";
+import Insta from "../Icon/InstaIcon";
+import Twitter from "../Icon/TwitterIcon";
+
+const style = css`
+  width: 32px;
+  height: 32px;
+  margin: 0 0 4px 4px;
+  border-radius: 12px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+`;
+
+function SocialButton({ social, uri }) {
+  return (
+    <a
+      css={style}
+      href={uri}
+      target="_blank"
+      rel="noreferrer noopener"
+      className="socials align-center"
+    >
+      {social == "github" && <Github />}
+      {social == "facebook" && <Facebook />}
+      {social == "linkedIn" && <LinkedIn />}
+      {social == "insta" && <Insta />}
+      {social == "twiiter" && <Twitter />}
+    </a>
+  );
 }
 
 export default SocialButton;
