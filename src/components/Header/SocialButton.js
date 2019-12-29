af44478ecd83c5147f20e2dab38658d73ea4aaec
Taek2 <zmn5119@gmail.com>
2019-12-29 23:04:22 +0900

    fix: Update social button style

--- src/components/Header/SocialButton.js
+++ src/components/Header/SocialButton.js
@@ -1,11 +1,15 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
-
-import Github from "../Icon/GithubIcon";
-import Facebook from "../Icon/FacebookIcon";
-import LinkedIn from "../Icon/LinkedInIcon";
-import Insta from "../Icon/InstaIcon";
-import Twitter from "../Icon/TwitterIcon";
+import Github from "../SVG/github.svg";
+import Facebook from "../SVG/facebook.svg";
+import LinkedIn from "../SVG/linked-in.svg";
+import Insta from "../SVG/insta.svg";
+import Twitter from "../SVG/twitter.svg";
+// import Github from "../Icons/GithubIcon";
+// import Facebook from "../Icons/FacebookIcon";
+// import LinkedIn from "../Icons/LinkedInIcon";
+// import Insta from "../Icons/InstaIcon";
+// import Twitter from "../Icons/TwitterIcon";
 
 const style = css`
   width: 32px;
@@ -15,6 +19,11 @@ const style = css`
   border: none;
   outline: none;
   cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+
+  svg {
+    transition: all 0.3s ease-in-out;
+  }
 `;
 
 function SocialButton({ social, uri }) {
