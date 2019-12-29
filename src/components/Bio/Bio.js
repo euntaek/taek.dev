ac80e2369ec1b9d37d353c2a943c6c4cab4dcb67
Taek2 <zmn5119@gmail.com>
2019-12-30 02:08:48 +0900

    feat: Add bio component styles

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -1,7 +1,42 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+
+const style = css`
+  margin: 8px 16px 48px;
+
+  p {
+    margin: 0;
+    font-size: 16px;
+    font-weight: normal;
+    letter-spacing: normal;
+
+    a {
+      position: relative;
+      text-decoration: none;
+    }
+    a:after {
+      content: "";
+      bottom: 1px;
+      left: 0px;
+      width: 2.75em;
+      position: absolute;
+      border-bottom: 1px solid;
+    }
+  }
+`;
 
 function Bio() {
-  return <aside>Bio</aside>;
+  const name = "김은택";
+  const test = "현재 프론트엔드 개발자를 목표로 공부하고 있습니다.";
+  return (
+    <aside id="bio" css={style}>
+      <p>
+        안녕하세요! <a href="#">{name}</a>이라고 합니다.
+        <br />
+        {test}
+      </p>
+    </aside>
+  );
 }
 
 export default Bio;
