c1db7f7148e0c8155a6d70e5033519f33bd7f429
Taek2 <zmn5119@gmail.com>
2019-12-29 23:06:53 +0900

    fix: Update header default styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,44 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
 
+import ThemeContainer from "./ThemeContainer";
 import SocialButton from "./SocialButton";
-import ThemeSwitch from "./ThemeSwitch";
+
+const style = css`
+  max-width: 100%;
+  display: flex;
+  /* flex-flow: row wrap; */
+  justify-content: space-between;
+  margin: 40px 16px 0;
+
+  h1 {
+    font-family: "Montserrat";
+    font-size: 48px;
+    font-weight: 800;
+    margin: 0;
+  }
+  div {
+    display: flex;
+    flex-flow: row-reverse wrap;
+  }
+`;
+
+const dumyDate = {
+  github: `https:github.com`,
+  facebook: `f`,
+  linkedIn: `i`,
+};
 
 function Header() {
   return (
-    <header>
-      <p>header</p>
-      <SocialButton />
-      <ThemeSwitch />
-      -------------------------------------------------------------------
+    <header css={style}>
+      <h1 className="title">TAEK.io</h1>
+      <div>
+        <ThemeContainer />
+        {Object.keys(dumyDate).map(item => (
+          <SocialButton social={item} uri={dumyDate[item]} key={item} />
+        ))}
+      </div>
     </header>
   );
 }
