d45bda1e6be43cb2d8600b49772f577e46cd81d5
Taek2 <zmn5119@gmail.com>
2019-12-29 23:05:57 +0900

    fix: Update Layout default styles

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,14 +1,21 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
 import Header from "../Header/Header";
 import Footer from "../Footer";
 
+const style = css`
+  min-width: 320px;
+  max-width: 696px;
+  margin: 0 auto;
+`;
+
 const Layout = ({ children }) => {
   return (
-    <>
+    <div css={style}>
       <Header />
       {children}
       <Footer />
-    </>
+    </div>
   );
 };
 
