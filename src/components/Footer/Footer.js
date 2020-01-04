49d48b36f31bee45a14e33e70e045f67d18c679f
Taek2 <zmn5119@gmail.com>
2020-01-05 00:46:58 +0900

    style: Add footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,20 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+
+const style = css`
+  margin-top: 40px;
+  width: 100%;
+  font-size: 14px;
+
+  a {
+    font-weight: bold;
+  }
+`;
 
 function Footer() {
   return (
-    <footer>
-      -------------------------------------------------------------------
-      <p>footer</p>
+    <footer id="footer" css={style} className="align-center">
+      ©{2020},&nbsp;<a href="#">Euntaek Kim</a>
     </footer>
   );
 }
