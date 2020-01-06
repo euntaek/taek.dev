ff8cb95800d72d8ed20b78658a8d14b4be3fd46e
Taek2 <zmn5119@gmail.com>
2020-01-07 02:16:19 +0900

    feat: Add about page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+
+import Bio from "../components/Bio";
+import Category from "../components/Category";
+import Contents from "../components/Contents";
+
+const index = () => {
+  return <Layout>about</Layout>;
+};
+
+export default index;
