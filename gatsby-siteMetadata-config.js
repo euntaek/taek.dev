8130881d9b2e11d0300b1a519695cc8a0b88be02
Taek2 <zmn5119@gmail.com>
2020-01-07 02:15:21 +0900

    fix: Upadate gatsby site metadata confg

--- gatsby-siteMetadata-config.js
+++ gatsby-siteMetadata-config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  title: `TAEK.io`,
-  author: `김은택`,
+  title: `TAEK LOG`,
+  author: { ko: `김은택`, en: `Euntaek Kim` },
+  siteUrl: `https://taek.io`,
   description: `A Blog by Euntaek Kim`,
   introduction: `현재 프론트엔드 개발자를 목표로 공부하고 있습니다.`,
-  siteUrl: `https://taek.io`,
   social: {
-    github: `EuntaekKim`,
+    github: `https://github.com/EuntaekKim`,
+    facebook: `https://www.facebook.com/profile.php?id=100002769998965`,
+    linkedIn: ``,
+    insta: `https://www.instagram.com/zmn5113/?hl=ko`,
+    twitter: ``,
   },
   GA: ``,
 };
