44f48b022c6479b489ab8ede1248d7ec85e3e794
Taek2 <zmn5119@gmail.com>
2020-01-10 02:09:26 +0900

    feat: Add date formatter

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,56 @@
+const path = require("path");
+const { createFilePath } = require("gatsby-source-filesystem");
 
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` });
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    });
+  }
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const result = await graphql(`
+    query {
+      allMarkdownRemark(
+        sort: { fields: [frontmatter___date], order: DESC }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.error) {
+    throw result.errors;
+  }
+
+  const posts = result.data.allMarkdownRemark.edges;
+  posts.forEach((post, index) => {
+    const previous = index === posts.length - 1 ? null : posts[index + 1].node;
+    const next = index === 0 ? null : posts[index - 1].node;
+
+    createPage({
+      path: post.node.fields.slug,
+      component: path.resolve(`./src/templates/blog-post.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
+    });
+  });
+};
