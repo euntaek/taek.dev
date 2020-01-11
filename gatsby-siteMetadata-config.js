c14b92ee87b971831892249f21f35a4cc49a46bc
Taek2 <zmn5119@gmail.com>
2020-01-11 22:30:37 +0900

    fix: Add site metadata keywords

--- gatsby-siteMetadata-config.js
+++ gatsby-siteMetadata-config.js
@@ -2,8 +2,20 @@ module.exports = {
   title: `TAEK LOG`,
   author: { ko: `김은택`, en: `Euntaek Kim` },
   siteUrl: `https://taek.io`,
-  description: `A Blog by Euntaek Kim`,
+  description: `김은택의 개인 블로그입니다. 개인적인 공부, 취미 등 이것저것 기록합니다.`,
   introduction: `현재 프론트엔드 개발자를 목표로 공부하고 있습니다.`,
+  icon: `content/assets/taek-icon.png`,
+  keywords: [
+    `blog`,
+    `development`,
+    `frontend`,
+    `web`,
+    `javascript`,
+    `typescript`,
+    `react`,
+    `html`,
+    `css`,
+  ],
   social: {
     github: `https://github.com/EuntaekKim`,
     facebook: `https://www.facebook.com/profile.php?id=100002769998965`,
@@ -11,5 +23,5 @@ module.exports = {
     insta: `https://www.instagram.com/zmn5113/?hl=ko`,
     twitter: ``,
   },
-  GA: ``,
+  ga: ``,
 };
