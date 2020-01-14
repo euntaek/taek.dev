738bdd01021b088357132685087142969ab73047
Taek2 <zmn5119@gmail.com>
2020-01-14 23:58:03 +0900

    fix:  Remove func toUpperCase()

--- src/utils/dateFormatter.js
+++ src/utils/dateFormatter.js
@@ -1,7 +1,7 @@
 import format from "date-fns/format";
 
 const dataFormatter = date => {
-  return format(new Date(date), "MMMM dd, yyyy").toUpperCase();
+  return format(new Date(date), "MMMM dd, yyyy");
 };
 
 export default dataFormatter;
