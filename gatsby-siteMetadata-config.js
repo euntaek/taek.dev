05cd58cb0ffdb7d91e8193b14edb6f5d76445441
Taek2 <zmn5119@gmail.com>
2020-01-17 03:26:59 +0900

    test: Change siteUrl

--- gatsby-siteMetadata-config.js
+++ gatsby-siteMetadata-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: `TAEK LOG`,
   author: { ko: `김은택`, en: `Euntaek Kim` },
-  siteUrl: `https://taek.io`,
+  siteUrl: `http://192.168.0.3:8000`,
   description: `김은택의 개인 블로그입니다. 개인적인 공부, 취미 등 이것저것 기록합니다.`,
   introduction: `현재 프론트엔드 개발자를 목표로 공부하고 있습니다.`,
   icon: `content/assets/taek-icon.png`,
