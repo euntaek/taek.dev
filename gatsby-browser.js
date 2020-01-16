0cff123474bde67f2b43bc908ded57a02cdb19e6
Taek2 <zmn5119@gmail.com>
2020-01-17 03:24:21 +0900

    feat: Add source code pro font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 require("typeface-rubik");
 require("typeface-roboto");
 require("typeface-noto-sans-kr");
+require("typeface-source-code-pro");
 require("swiper/css/swiper.min.css");
 require("./src/styles/global.scss");
