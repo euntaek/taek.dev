dbf326b313333dc929b0b8128dfb0c5f2bfc2512
Taek2 <zmn5119@gmail.com>
2020-01-20 17:45:21 +0900

    fix: Update keywords

--- gatsby-siteMetadata-config.js
+++ gatsby-siteMetadata-config.js
@@ -5,17 +5,7 @@ module.exports = {
   description: `김은택의 개인 블로그입니다. 개인적인 공부, 취미 등 이것저것 기록합니다.`,
   introduction: `현재 프론트엔드 개발자를 목표로 공부하고 있습니다.`,
   icon: `content/assets/taek-icon.png`,
-  keywords: [
-    `blog`,
-    `development`,
-    `frontend`,
-    `web`,
-    `javascript`,
-    `typescript`,
-    `react`,
-    `html`,
-    `css`,
-  ],
+  keywords: [`blog`, `development`, `frontend`],
   social: {
     github: `https://github.com/EuntaekKim`,
     facebook: `https://www.facebook.com/profile.php?id=100002769998965`,
