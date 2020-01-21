87feed2c503576dd61e95e86e9b98f01a11f842a
Taek2 <zmn5119@gmail.com>
2020-01-21 14:03:36 +0900

    fix: Add seo to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Layout from "../components/Layout";
+import SEO from "../components/SEO";
 
-function NotFoundPage() {
-  return <Layout>잘못 된 경로입니다.</Layout>;
+function NotFoundPage({ location }) {
+  return (
+    <Layout location={location}>
+      <SEO title="404: Not found" url="/404" />
+      <h1>404!</h1>
+      <p>없는 페이지입니다.</p>
+    </Layout>
+  );
 }
 
 export default NotFoundPage;
