ea9bc98389f2d5d2c49ae88ee02fb85a9dabff65
Euntaek Kim <zmn5119@gmail.com>
2021-09-10 20:04:38 +0900

    update: 소개 수정

--- gatsby-siteMetadata.js
+++ gatsby-siteMetadata.js
@@ -3,7 +3,7 @@ module.exports = {
   author: { ko: `김은택`, en: `Euntaek Kim` },
   siteUrl: `https://taek.io`,
   description: `김은택의 개인 블로그입니다. 개인적인 공부, 취미 등을 기록합니다.`,
-  introduction: `현재 프론트엔드 개발자를 목표로 공부하고 있습니다.`,
+  introduction: `안녕하세요! 프론트엔드 개발자 김은택입니다.`,
   aboutUrl: "https://www.notion.so/1766de626e274aa584d0e0cd3fcd4abe",
   icon: `content/assets/taek-icon.png`,
   keywords: [`blog`, `development`, `frontend`],
